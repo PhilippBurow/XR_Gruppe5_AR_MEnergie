8ca98140b0b85b8321952052a5ca2a2381d36de2
Daniel Koeln <daniel.koeln@smail.th-koeln.de>
2022-01-06 18:08:51 +0100

    Spawnen Windturbinen
    Problem: Windturbine sehr groß, werden direkt vor der Kamera gespawned. können nicht mit einer Entfernung gespawned werden

---
+++ XR_Gruppe5_AR_MEnergie/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject WindTurbinePrefab;
+    public Camera ARCamera;
+    public GameObject SpawnedWindTurbine;
+
+    private List<RaycastResult> raycastResults = new List<RaycastResult>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse down");
+
+            //Position wo Bildschirm gedrückt wird eritteln 
+            Ray ray = ARCamera.ScreenPointToRay(Input.mousePosition);
+            //Debug Funktion ist zur Fehlerermittlung hilfreich. Es handelt sich um eine Ausgabe in der Konsole
+            Debug.Log(ray);
+
+            if (isPointerOverUI(Input.mousePosition))
+            {
+                Debug.Log("Do nothing!");
+            }
+            else
+            {
+                SpawnedWindTurbine = Instantiate(WindTurbinePrefab, ray.origin, Quaternion.identity);
+            }
+        }
+    }
+
+    private bool isPointerOverUI(Vector2 fingerPosition)
+    {
+        PointerEventData eventDataPosition = new PointerEventData(EventSystem.current);
+        eventDataPosition.position = fingerPosition;
+        EventSystem.current.RaycastAll(eventDataPosition, raycastResults);
+        return raycastResults.Count > 0; //if greater than zero, that means we hit a UI Element
+    }
+}
