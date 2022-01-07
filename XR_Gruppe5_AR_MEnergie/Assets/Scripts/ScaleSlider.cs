e0f8746b6bd9374410b6d01495935d4d5d85ee80
Philipp Burow <philipp_johann.burow@smail.th-koeln.de>
2022-01-07 10:30:42 +0100

    ScaleSlider & PlaneHiding
    ScaleSlider integriert um höhe von Windrädern im game ändern zu können
    Plane Hiding, um im game die erkannten Oberflächen ein/ausblenden zu können

---
+++ XR_Gruppe5_AR_MEnergie/Assets/Scripts/ScaleSlider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleSlider : MonoBehaviour
+{
+    // Erstellen einer Variable 
+
+    private Slider scaleSlider;
+
+
+    void Start()
+    {
+        // Slider in Hierarchy finden
+        scaleSlider = GameObject.Find("ScaleSlider").GetComponent<Slider>();
+        scaleSlider.onValueChanged.AddListener(ScaleSliderUpdate);
+    }
+
+    // Einheitliche Variable für alle drei Scale-Variablen erstellen und diese mit dem ScaleSlider verknuepfen
+    void ScaleSliderUpdate(float value)
+    {
+        transform.localScale = new Vector3(value, value, value);
+            
+            }
+    void Update()
+    {
+       
+    }
+}
