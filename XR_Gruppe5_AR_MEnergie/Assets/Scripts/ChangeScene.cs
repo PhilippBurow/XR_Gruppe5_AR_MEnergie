2f85c0da408995316939b264226eb1fc44c7a2d9
Daniel Koeln <daniel.koeln@smail.th-koeln.de>
2022-01-23 13:24:28 +0100

    Smog Scene hinzugefügt

--- XR_Gruppe5_AR_MEnergie/Assets/Scripts/ChangeScene.cs
+++ XR_Gruppe5_AR_MEnergie/Assets/Scripts/ChangeScene.cs
@@ -6,19 +6,21 @@ using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
-    public Toggle Renewable;
-    public Toggle Desaster;
+    public Toggle OneSceneUp;
+    public Toggle FourScenesUp;
+    public Toggle Home;
+    public Toggle Help;
     
     //Wenn die für Erneuerbare Energien zuständigen Buttons betätigt werden, wir auf den Index der Scene 1 addiert. Bei Buttons
     //für Umweltkatastrophen der Index 3, um auf den richten Screen zu kommen (s. Build Settings)
     public void ChangeScreen()
     {
-        if (Renewable.isOn)  
+        if (OneSceneUp.isOn || SceneManager.GetActiveScene().name == "DisasterScene")  
         {
             Debug.Log("plus 1");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-        else if (Desaster.isOn)
+        else if (FourScenesUp.isOn)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
         }
@@ -27,19 +29,19 @@ public class ChangeScene : MonoBehaviour
     //befinden man sich auf der Startseite für Umweltkatastrophen, muss 3 Indize zurück gegangen werden. (s. Build Settings)
     public void Back()
     {
-        Debug.Log("Active Scene : " + SceneManager.GetActiveScene().name);
-        if (SceneManager.GetActiveScene().name == "StartSceneEE" || SceneManager.GetActiveScene().name == "InfoScene")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else if (SceneManager.GetActiveScene().name == "ARScene")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-        }
-        else if(SceneManager.GetActiveScene().name == "DisasterScene")
+        if (SceneManager.GetActiveScene().name == "DisasterScene")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
         }
+        if (SceneManager.GetActiveScene().name == "StartSceneEE" || SceneManager.GetActiveScene().name == "InfoScene" || SceneManager.GetActiveScene().name == "SmogScene" || Help.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        if (Home.isOn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+        
     }
     //hier wird die Anwendung beendet
     public void Quit()
